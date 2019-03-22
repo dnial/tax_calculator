9f346931d5ab9f9977706542db4936ca8f064f0c
Daniel Hendrianto <gracyandi@ruma.co.id>
2019-03-22 12:40:45 +0700

    update db fields

--- tax_calculator/calculator/models.py
+++ tax_calculator/calculator/models.py
@@ -1,6 +1,5 @@
-from django.urls import reverse
-from django.utils.translation import ugettext_lazy as _
 from django.db import models
+from django.forms import ModelForm
 
 
 class Codes(models.Model):
@@ -9,17 +8,18 @@ class Codes(models.Model):
     '''
 
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=70, null=True, blank=True)
+    name = models.CharField(max_length=70)
     tax_pct = models.DecimalField(max_digits=6, decimal_places=4, null=False)
     tax_pct_2 = models.DecimalField(max_digits=6, decimal_places=4, null=True)
     is_refundable = models.BooleanField(default=False)
     non_taxable_limit = models.DecimalField(max_digits=20, decimal_places=4, default=0, null=False)
 
+
 class Users(models.Model):
     '''
     Table For Users
     '''
-    name = models.CharField(max_length=70, null=True, blank=True)
+    name = models.CharField(max_length=70)
 
 
 class Items(models.Model):
@@ -28,6 +28,5 @@ class Items(models.Model):
     '''
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
     code = models.ForeignKey(Codes, on_delete=models.CASCADE)
-    name = models.CharField(max_length=70, null=True, blank=True)
-    price = models.DecimalField(max_digits=20, decimal_places=4, null=True, blank=True)
-
+    name = models.CharField(max_length=70)
+    price = models.DecimalField(max_digits=20, decimal_places=4)
